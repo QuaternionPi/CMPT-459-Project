c6d29cb5abe8b7c1812b6a0321c28ef196f0db9e
David Wiebe <drwho.david.wiebe@gmail.com>
2024-11-02 17:36:01 -0700

    Add writer to preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
+from writer import Writer
 
 
-def preprocess(path: str) -> pd.DataFrame:
+def preprocess(path: str, writer: Writer) -> pd.DataFrame:
     """
     Preprocess data.
 
-    :param path: Path to data file (csv)
-    :return: Pandas data frame of kept data
+    :param path: Path to data file (csv).
+    :param writer: Where to write outputs.
+    :return: Pandas data frame of kept data.
     """
     df: pd.DataFrame = pd.read_csv(path)
     return df
