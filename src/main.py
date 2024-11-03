eb36f6fa9ddfb5139b0bbf820b47ab9e237c77d7
David Wiebe <drwho.david.wiebe@gmail.com>
2024-11-03 11:58:57 -0800

    Add call to preprocessing to main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+import pandas as pd
+import numpy as np
 import argparse
+from writer import Writer
+from preprocessing import preprocess
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 
 def parse_args() -> tuple[bool, str]:
@@ -19,12 +25,22 @@ def parse_args() -> tuple[bool, str]:
     return (args.verbose, args.data)
 
 
+def split(data: pd.DataFrame, test_ratio: int) -> tuple[pd.DataFrame, pd.DataFrame]:
+    test_size = 1 / test_ratio
+    return train_test_split(data, test_size=test_size)
+
+
 def main() -> None:
     """
     Main function of the program
     """
-    (verbose, data) = parse_args()
+    (verbose, path) = parse_args()
+    writer: Writer = Writer(verbose, None)
+
+    data = preprocess(path, writer)
+    test_ratio = 5
+    train, test = split(data, test_ratio=test_ratio)
 
 
-if __name__ == "__main_":
+if __name__ == "__main__":
     main()
