de25fdfeb8a80abbb17a0025b39ba5e3d12cc5f1
David Wiebe <drwho.david.wiebe@gmail.com>
2024-11-19 13:57:51 -0800

    Implement recursive feature elimination

--- src/feature_selection.py
+++ src/feature_selection.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.svm import SVR
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import Lasso
 from sklearn.metrics import mutual_info_score
@@ -12,7 +13,16 @@ def recursive_feature_elimination(data: pd.DataFrame, writer: Writer) -> pd.Data
 
     :return: feature-reduced data frame.
     """
-    pass
+    estimator = SVR(kernel="linear")
+    selector = RFE(estimator, n_features_to_select=5, step=1)
+    y = data["Rainfall"]
+    X = data.drop(columns=["Rainfall"], inplace=False)
+    selector = selector.fit(X, y)
+
+    dropped_columns = [
+        col for col, keep in zip(data.columns, selector.support_) if keep
+    ]
+    return data.drop(columns=dropped_columns, inplace=False)
 
 
 def lasso_regression(data: pd.DataFrame, writer: Writer) -> pd.DataFrame:
