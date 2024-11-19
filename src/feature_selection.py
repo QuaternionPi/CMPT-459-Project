660fca7fb64ba7024a80f9de0cffb514dbe4d770
David Wiebe <drwho.david.wiebe@gmail.com>
2024-11-19 14:23:10 -0800

    Implement lasso regression

--- src/feature_selection.py
+++ src/feature_selection.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+from sklearn.discriminant_analysis import StandardScaler
 from sklearn.svm import SVR
-from sklearn.feature_selection import RFE
+from sklearn.feature_selection import RFE, SelectFromModel
 from sklearn.linear_model import Lasso
 from sklearn.metrics import mutual_info_score
 from writer import Writer
@@ -13,14 +14,16 @@ def recursive_feature_elimination(data: pd.DataFrame, writer: Writer) -> pd.Data
 
     :return: feature-reduced data frame.
     """
-    estimator = SVR(kernel="linear")
-    selector = RFE(estimator, n_features_to_select=5, step=1)
     y = data["Rainfall"]
     X = data.drop(columns=["Rainfall"], inplace=False)
+
+    estimator = SVR(kernel="linear")
+
+    selector = RFE(estimator, n_features_to_select=5, step=1)
     selector = selector.fit(X, y)
 
     dropped_columns = [
-        col for col, keep in zip(data.columns, selector.support_) if keep
+        col for col, keep in zip(data.columns, selector.get_support()) if keep
     ]
     return data.drop(columns=dropped_columns, inplace=False)
 
@@ -31,7 +34,21 @@ def lasso_regression(data: pd.DataFrame, writer: Writer) -> pd.DataFrame:
 
     :return: feature-reduced data frame.
     """
-    pass
+    y = data["Rainfall"]
+    X = data.drop(columns=["Rainfall"], inplace=False)
+
+    estimator = StandardScaler()
+    estimator.fit(X)
+
+    lasso = Lasso(alpha=0.2)
+
+    selector = SelectFromModel(lasso)
+    selector = selector.fit(estimator.transform(X), y)
+
+    dropped_columns = [
+        col for col, keep in zip(data.columns, selector.get_support()) if keep
+    ]
+    return data.drop(columns=dropped_columns, inplace=False)
 
 
 def mutual_information(data: pd.DataFrame, writer: Writer) -> pd.DataFrame:
