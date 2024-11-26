f537a6a30663204fa6f09a692bb925d7bf2176ed
David Wiebe <drwho.david.wiebe@gmail.com>
2024-11-26 13:54:30 -0800

    Update type signatures

--- src/outlier_detection.py
+++ src/outlier_detection.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from writer import Writer
 from exploratory_analysis import Analyzer
+from sklearn.neighbors import KernelDensity
+from sklearn.neighbors import LocalOutlierFactor
 
 
 class OutlierDetection:
-    def __init__(self, lof: float, data: pd.DataFrame, writer: Writer):
+    def __init__(
+        self, lof: float, bandwidth: float, data: pd.DataFrame, writer: Writer
+    ):
         """
         Determines if data points are outliers by lof
 
@@ -13,10 +17,11 @@ class OutlierDetection:
         :param writer: Where to write outputs.
         """
         self.lof: float = lof
+        self.bandwidth: float = bandwidth
         self.data: pd.DataFrame = data
         self.writer: Writer = writer
 
-    def find_outliers() -> pd.DataFrame:
+    def find_outliers(self) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
         Return a data frame with a column identifying outliers.
 
@@ -24,17 +29,17 @@ class OutlierDetection:
         """
         pass
 
-    def data_without_outliers() -> pd.DataFrame:
+    def data_without_outliers(self) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
         Return a data frame without outliers.
 
         :return: Data frame without outliers.
         """
 
-    def visualize() -> tuple[Analyzer, Analyzer]:
+    def visualize(self) -> tuple[Analyzer, Analyzer]:
         """
         Return pair of visualizers trained on PCA data showing outliers.
 
-        :return: Pair of 2d and 3d analyzers trained on PCA data showing outliers.
+        :return: Pair of 2d and analyzers trained on PCA data showing outliers.
         """
         pass
